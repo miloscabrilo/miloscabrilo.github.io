22ba19d85ffc7ef92c0fba4caff6e90541db2daf
Milos Cabrilo <milos.cabrilo@neon-free.ch>
2025-05-22 11:45:13 +0200

    Integrated popover in header. User can click on three dots and open popover. Added two items into popover: language and theme.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'demetra-header',
-  imports: [TranslateModule],
+  imports: [TranslatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isPopoverOpen: boolean = false;
+
+  @ViewChild('popover') popover!: ElementRef;
+  @ViewChild('triggerButton') triggerButton!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  private handleClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (
+      this.isPopoverOpen &&
+      !this.popover.nativeElement.contains(target) &&
+      !this.triggerButton.nativeElement.contains(target)
+    ) {
+      this.isPopoverOpen = false;
+    }
+  }
+
+  public togglePopover() {
+    this.isPopoverOpen = !this.isPopoverOpen;
+  }
+
+  public openLanguageModal(): void {
+    console.log('Language modal opened');
+    this.togglePopover();
+  }
+  public openThemeModal(): void {
+    console.log('Theme modal opened');
+    this.togglePopover();
+  }
+}
