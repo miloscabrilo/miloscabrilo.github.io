a6aed4f456310b02a899f720f296ccf2a2795ee0
Milos Cabrilo <mlscabrilo5@gmail.com>
2026-02-18 00:36:14 +0100

    Added captcha field into contact us form.

--- src/app/components/contact-modal/contact-modal.component.ts
+++ src/app/components/contact-modal/contact-modal.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, effect, inject, input, output, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ThemeService } from '../../core/services/theme.service';
 import { Theme } from '../../core/enums/theme.enum';
@@ -18,12 +18,27 @@ export class ContactModalComponent {
   readonly isVisible = input<boolean>(false);
   readonly close = output<void>();
 
+  readonly captchaA = signal(0);
+  readonly captchaB = signal(0);
+
   readonly contactForm = this.fb.group({
     name: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
     phone: ['', Validators.required],
     message: ['', Validators.required],
+    captcha: ['', [Validators.required, (c: AbstractControl): ValidationErrors | null =>
+      c.value && Number(c.value) === this.captchaA() + this.captchaB() ? null : { captcha: true }
+    ]],
   });
+
+  constructor() {
+    this.generateCaptcha();
+    effect(() => {
+      if (this.isVisible()) {
+        this.generateCaptcha();
+      }
+    });
+  }
 
   closeModal(): void {
     this.contactForm.reset();
@@ -48,5 +63,10 @@ export class ContactModalComponent {
       default:
         return 'assets/icons/light/close-24x24.svg';
     }
+  }
+
+  private generateCaptcha(): void {
+    this.captchaA.set(Math.floor(Math.random() * 10) + 1);
+    this.captchaB.set(Math.floor(Math.random() * 10) + 1);
   }
 }
